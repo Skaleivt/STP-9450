1f015ba8ac24c99931cb9439efbaf7bc14f72e64
Denys Koval <metalheadden@gmail.com>
2025-06-21 23:16:02 +0300

    reviews_connect_contact_markup

---
+++ src/js/swiper.js
@@ -0,0 +1,31 @@
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const swiper = new Swiper('.swiper', {
+      // Optional parameters
+  modules: [Pagination],
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 40,
+  initialSlide: 4,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    320: {
+      direction: 'horizontal',
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+    1200: {
+      direction: 'vertical',
+      slidesPerView: 10,
+      spaceBetween: 26
+    },
+  }
+});
